c008ebe97acaa10c3f15e8352462989bbe9e561b
taylarogers <110628845+taylarogers@users.noreply.github.com>
2023-05-16 22:19:28 +0200

    Turnaround Times

--- TurnaroundTimes.py
+++ TurnaroundTimes.py
@@ -1,45 +1,65 @@
-startTimes = []
-endTimes = []
-turnaroundTimes = []
-aveTurnaroundTime = 0
+import os
 
-# Load data from file
-with open ('output/ContextSwitch/FCFS_4_5.txt') as file:
-    lines = file.readlines()
-    
-    data = []
+def assessFile(filename):
+    startTimes = []
+    endTimes = []
+    turnaroundTimes = []
+    aveTurnaroundTime = 0
 
-    for line in lines:
-        data.append(line.split(", "))
-    
-# Find the start time for each process
-for i in range(1,18+1):
-    pid = str(i).zfill(3)
+    # Load data from file
+    with open (filename) as file:
+        lines = file.readlines()
+        
+        data = []
 
-    for action in data:
-        if action[0] == pid:
-            startTimes.append(action[3])
-            break
+        for line in lines:
+            data.append(line.split(", "))
+        
+    # Find the start time for each process
+    for i in range(1,18+1):
+        pid = str(i).zfill(3)
 
-# Find the end time for each process
-for i in range(18,0,-1):
-    pid = str(i).zfill(3)
+        for action in data:
+            if action[0] == pid:
+                startTimes.append(action[3])
+                break
 
-    for action in data[::-1]:
-        if action[0] == pid:
-            endTimes.insert(0, action[3])
-            break
+    # Find the end time for each process
+    for i in range(18,0,-1):
+        pid = str(i).zfill(3)
 
-# Find average turnaround time among the processes
-totalTime = 0
+        for action in data[::-1]:
+            if action[0] == pid:
+                endTimes.insert(0, action[3])
+                break
 
-for i in range(0, 18):
-    turnaroundTimes.append(int(endTimes[i]) - int(startTimes[i]))
-    totalTime += turnaroundTimes[i]
+    # Find average turnaround time among the processes
+    totalTime = 0
 
-aveTurnaroundTime = round(totalTime / 18)
+    for i in range(0, 18):
+        turnaroundTimes.append(int(endTimes[i]) - int(startTimes[i]))
+        totalTime += turnaroundTimes[i]
 
-# Write to file
-file = open("TurnaroundTimes_ContextSwitch.txt", "a")
-file.write("FCFS_4_5 ContextSwitch : " + str(aveTurnaroundTime) + "\n")
-file.close()
+    aveTurnaroundTime = round(totalTime / 18)
+
+    # Get dataset name
+    indexLastSlash = filename.rfind("/")
+    indexFirstSlash = filename.find("/")
+    indexDot = filename.rfind(".")
+    directory = filename[int(indexFirstSlash+1):int(indexLastSlash)]
+    datasetName = filename[int(indexLastSlash+1):int(indexDot)]
+
+    # Write to file
+    outputFilename = "TurnaroundTimes_" + directory + ".txt"
+    file = open(outputFilename, "a")
+    file.write(datasetName + " " + directory + " : " + str(aveTurnaroundTime) + "\n")
+    file.close()
+
+def getFiles(directory):
+    path = "output/" + directory + "/"
+
+    for file in os.listdir(path):
+        assessFile("output/" + directory + "/" + file)
+
+getFiles("ContextSwitch")
+getFiles("TimeQuantum")
